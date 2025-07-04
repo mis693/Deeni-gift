8a225df5749bef45f8cbaa332a5f4d3ba073b2e4
mis693 <37755583+mis693@users.noreply.github.com>
2025-07-05 00:08:45 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,11 @@
+// pages/index.js
+import GiftSenderSection from '../components/GiftSenderSection';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black">
+      <GiftSenderSection />
+    </main>
+  );
+}
 
